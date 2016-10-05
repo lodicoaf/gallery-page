a1412de7158833567a25112c8ec57c0fd1ea011f
Alex LoDico <alexlodico@Alexs-MacBook-Pro.local>
2016-10-05 18:27:10 -0400

    adding pictures and formatting popup

--- bxslider/slider.js
+++ bxslider/slider.js
@@ -7,3 +7,39 @@ $(document).ready(function(){
 	  pause: 5000
 	});
 });
+
+$(document).ready(function(){
+	$('.bxslider2').bxSlider({
+	  mode: 'fade',
+	  auto: false,
+	  autoControls: true,
+	  pause: 2000
+	});
+});
+
+$(document).ready(function(){
+	$('.bxslider3').bxSlider({
+	  mode: 'fade',
+	  auto: false,
+	  autoControls: true,
+	  pause: 2000
+	});
+});
+
+$(document).ready(function(){
+	$('.bxslider4').bxSlider({
+	  mode: 'fade',
+	  auto: false,
+	  autoControls: true,
+	  pause: 2000
+	});
+});
+
+$(document).ready(function(){
+	$('.bxslider5').bxSlider({
+	  mode: 'fade',
+	  auto: false,
+	  autoControls: true,
+	  pause: 2000
+	});
+});
