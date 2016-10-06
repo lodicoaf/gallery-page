79d5d981222ad54e75d59f09ae91284ccf0f5900
Alex LoDico <alexlodico@Alexs-MacBook-Pro.local>
2016-10-06 10:49:53 -0400

    i don't know

--- bxslider/slider.js
+++ bxslider/slider.js
@@ -1,45 +1,9 @@
 $(document).ready(function(){
 	$('.bxslider').bxSlider({
-	  mode: 'fade',
+	  mode: 'horizontal',
 	  captions: true,
 	  auto: true,
 	  autoControls: false,
-	  pause: 5000
-	});
-});
-
-$(document).ready(function(){
-	$('.bxslider2').bxSlider({
-	  mode: 'fade',
-	  auto: false,
-	  autoControls: true,
-	  pause: 2000
-	});
-});
-
-$(document).ready(function(){
-	$('.bxslider3').bxSlider({
-	  mode: 'fade',
-	  auto: false,
-	  autoControls: true,
-	  pause: 2000
-	});
-});
-
-$(document).ready(function(){
-	$('.bxslider4').bxSlider({
-	  mode: 'fade',
-	  auto: false,
-	  autoControls: true,
-	  pause: 2000
-	});
-});
-
-$(document).ready(function(){
-	$('.bxslider5').bxSlider({
-	  mode: 'fade',
-	  auto: false,
-	  autoControls: true,
-	  pause: 2000
+	  pause: 4000
 	});
 });
